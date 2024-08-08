8a8a8c8b5faa8f8a5fdb9f5613045b8958f9107d
Azeez Adebayo <debaycisse@gmail.com>
2024-08-09 00:55:07 +0100

    implement task 6
    6. Add bonus
    script creates a stored procedure AddBonus that adds a new correction for a student

---
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,19 @@
+-- creates a stored procedure AddBonus that adds a new correction for a student.
+DELIMITER ##
+
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+	DECLARE proj_id INT;
+
+	SELECT id INTO proj_id FROM projects WHERE name = project_name LIMIT 1;
+
+	IF proj_id IS NULL THEN
+		INSERT INTO projects (name) VALUES (project_name);
+		SET proj_id = LAST_INSERT_ID();
+	END IF;
+
+	INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, proj_id, score);
+
+END ##
+
+DELIMITER ;
