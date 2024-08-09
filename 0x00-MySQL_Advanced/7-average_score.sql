41a14c9434465e7a6d18ccf78bc6d71b43f93af3
Azeez Adebayo <debaycisse@gmail.com>
2024-08-09 11:20:36 +0100

    apply a workarround to correctly select fields

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -3,15 +3,11 @@ DELIMITER ##
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
 
-DECLARE av_score DECIMAL(10, 2);
-DECLARE total_scores INT;
-DECLARE score_counts INT;
+DECLARE av_scores INT;
 
-SELECT SUM(score) INTO total_scores FROM corrections WHERE user_id = user_id;
-SELECT COUNT(score) INTO score_counts FROM corrections WHERE user_id = user_id;
-SET av_score = total_scores / score_counts;
+SELECT AVG(score) INTO av_scores FROM corrections AS cr WHERE cr.user_id = user_id;
 
-UPDATE users SET average_score = av_score WHERE id = user_id;
+UPDATE users SET average_score = av_scores WHERE id = user_id;
 
 END ##
 DELIMITER ;
