c903c3bd90e2b042e15e3f21877385ddf9e2fffc
Azeez Adebayo <debaycisse@gmail.com>
2024-08-10 05:27:17 -0700

    enable strict condition, using parenthesis

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,2 +1,2 @@
 -- script creates a view, named need_meeting that lists all students that have a score under 80 and 
-CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND last_meeting IS NULL OR last_meeting > ADDDATE(CURDATE(), INTERVAL -1 MONTH);
+CREATE VIEW need_meeting AS SELECT name FROM students WHERE score < 80 AND ((last_meeting IS NULL) OR (last_meeting > ADDDATE(CURDATE(), INTERVAL -1 MONTH)));
