5a1aa56274f57bdbb16dcd7304ca0b72e1122b3d
Azeez Adebayo <debaycisse@gmail.com>
2024-08-12 22:47:34 +0100

    implement task 11
    11. Where can I learn Python?
    a Python function that returns the list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,16 @@
+#!/usr/vin/env python3
+"""This module houses the definition of a function, named schools_by_topic"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """
+    returns a list of school documents that have a specific topic
+
+    Args:
+        mongo_collection - the collection to lookup for the documents
+        topic - the specific attribute of a school document to look for
+
+    Returns:
+        list of all school document that contains the topic
+    """
+    return mongo_collection.find({'topics': topic})
