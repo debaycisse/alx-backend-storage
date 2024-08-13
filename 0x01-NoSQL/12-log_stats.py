f7d7851ba7d277c08f3fc4b9d4fae85daeb02664
Azeez Adebayo <debaycisse@gmail.com>
2024-08-13 14:52:00 +0100

    apply a new logic to extract and print the data

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,26 +1,18 @@
 #!/usr/bin/env python3
 """This module provides some statistics from the collection,
 named nginx in a database, named logs"""
-
 import pymongo
 
 client = pymongo.MongoClient()
 db = client['logs']
 nginx_col = db['nginx']
-get_method_count = nginx_col.count_documents({'method': 'GET'})
-post_method_count = nginx_col.count_documents({'method': 'POST'})
-put_method_count = nginx_col.count_documents({'method': 'PUT'})
-patch_method_count = nginx_col.count_documents({'method': 'PATCH'})
-delete_method_count = nginx_col.count_documents({'method': 'DELETE'})
-status_checked = nginx_col.count_documents({
-                                            'method': 'GET',
+status_checked = nginx_col.count_documents({'method': 'GET',
                                             'path': '/status'})
+methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
 
 print('{} logs'.format(nginx_col.count_documents({})))
 print('Methods:')
-print('    method GET: {}'.format(get_method_count))
-print('    method POST: {}'.format(post_method_count))
-print('    method PUT: {}'.format(put_method_count))
-print('    method PATCH: {}'.format(patch_method_count))
-print('    method DELETE: {}'.format(delete_method_count))
+for method in methods:
+    counter = nginx_col.count_documents({'method': method})
+    print('\tmethod {}: {}'.format(method, counter))
 print('{} status check'.format(status_checked))
