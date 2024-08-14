dfdd890812aaabde4ac89836d21d220ecbed2a58
Azeez Adebayo <debaycisse@gmail.com>
2024-08-14 17:16:29 +0100

    implement task 1
    1. Reading from Redis and recovering original type
    add a method, named get, which gets a value via its key and returns
    the value its original form, rather than in bytes' form

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -27,3 +27,46 @@ class Cache:
         k: str = str(uuid4())
         self._redis.set(k, data)
         return k
+
+    def get_str(self, data: bytes) -> str:
+        """
+        converts a given data to string format or type
+
+        Args:
+            data - the given data to be converted
+
+        Returns:
+            the string equivalent of the passed data
+        """
+        return str(data)
+
+    def get_int(self, data: bytes) -> int:
+        """
+        coverts a given data to an interger form or type
+
+        Args:
+            data - the given data to be converted into an integer type
+
+        Returns:
+            the integer equivalent of the passed or given data
+        """
+        return int(data)
+
+    def get(self, key: str,
+            fn: Union[Callable[[bytes], Union[str, int]], None] = None
+            ) -> Union[None, str, int, bytes]:
+        """
+        retrieves the value (in its original form other than bytes),
+        stored in the passed key
+
+        Args:
+            key - the key to the value that is to be retrieved
+            fn - a function that should be used for the
+            conversion of the retrieved data whose key is passed
+        """
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if value is not None and fn is not None:
+            return fn(value)
+        return value
