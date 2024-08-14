f9a44d5528d312e7333a8bba8770fd1d393007c4
Azeez Adebayo <debaycisse@gmail.com>
2024-08-14 14:31:56 +0100

    implement task 0
    0. Writing strings to Redis
    defines a class with a method through which one can store data into a Redis database

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""This module houses the definition of a class, named Cache"""
+
+import redis
+from uuid import uuid4
+from typing import Union
+
+
+class Cache:
+    """This class writes a given string to Redis storage"""
+
+    data_types = Union[bytes, str, int, float]
+
+    def __init__(self):
+        """Method creates an instance of Redis and empties it"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: data_types) -> str:
+        """Method stores a given data in redis, using
+        randomly generated uuid as a key
+
+        Args:
+            data - the data to be stored in the redis
+
+        Returns:
+            the randomly generated key, which is used for storing data
+        """
+        k = str(uuid4())
+        self._redis.set(k, data)
+        return k
